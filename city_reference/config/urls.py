d32543089db7da8b662b6a493dc415a4d13b8d89
Ivan <vanopronin94@gmail.com>
2025-09-28 16:57:04 +0300

    добавил города

--- city_reference/config/urls.py
+++ city_reference/config/urls.py
@@ -5,5 +5,10 @@ from core.views import main
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', main)
+    path('', main),
+    path("moscow",main, name = "Москва"),
+    path("spb",main, name = "Санкт-Петербург"),
+    path("saratov", main, name = "Саратов"),
+    path("kazan", main, name = "Казань"),
+    path("samara", main, name = "Самара")
 ]
--- city_reference/core/views.py
+++ city_reference/core/views.py
@@ -2,4 +2,10 @@ from django.shortcuts import render
 
 
 def main(request):
-    return render(request, 'main.html')
+    cities=["Москва","Санкт-Петербург","Саратов","Казань","Самара"]
+    path=request.path
+    path=path.replace("/","")
+    if path == "":
+        path="main"
+    print(path)
+    return render(request, f'{path}.html',{"cities":cities})
