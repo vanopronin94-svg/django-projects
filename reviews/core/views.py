226d26c5b74cfaf518776f69186a8174c6116c4c
Ivan <vanopronin94@gmail.com>
2025-10-30 18:56:34 +0300

    двалпрвлопы

--- reviews/core/views.py
+++ reviews/core/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    print(request.GET)
+    name=request.GET.get("name")
+    email=request.GET.get("email")
+    rating=request.GET.get("rating")
+    review=request.GET.get("review")
+    return render(request,"index.html",{"name":name,"email":email,"rating":rating,"review":review})
